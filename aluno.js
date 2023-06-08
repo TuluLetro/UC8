614a7a2a50b85eef5d11fd055c30205f2e5c639e
TuluLetro <101512157+TuluLetro@users.noreply.github.com>
2023-06-08 17:44:58 -0300

    Add files via upload

---
+++ aluno.js
@@ -0,0 +1,18 @@
+
+let numeroDeAlunos = ["a", "b",, "d", "e"]
+for (let contador = 0; contador < numeroDeAlunos.length; contador++) {
+
+    if (contador == 0) {
+        console.log("Zero")
+
+    }else if (contador % 2 == 0) {
+        console.log(`O numero ${contador} é par`)
+
+    }else{
+        console.log(`O numero ${contador} é impar`)
+    }
+}
+
+
+
+
